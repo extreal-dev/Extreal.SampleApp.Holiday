aed849982b3e1e785661a3ba5b6c44769f9047f7
kiyohome <kiyohito.itoh@gmail.com>
2022-10-31 15:10:00 +0900

    RuntimeInitializeOnLoadMethodでフレームレートとログの初期化を追加

---
+++ Assets/Holiday/Common/Initializer.cs
@@ -0,0 +1,21 @@
+using Extreal.Core.Logging;
+using UnityEngine;
+
+public static class Initializer
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+
+        const LogLevel logLevel = LogLevel.Debug;
+        LoggingManager.Initialize(logLevel: logLevel);
+
+        var logger = LoggingManager.GetLogger(nameof(Initializer));
+        if (logger.IsDebug())
+        {
+            logger.LogDebug($"targetFrameRage: {Application.targetFrameRate}, logLevel: {logLevel}");
+        }
+    }
+}
