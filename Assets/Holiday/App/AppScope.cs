44644581ae07575e95975ff94659d958e77c8688
kiyohome <kiyohito.itoh@gmail.com>
2022-11-27 10:50:19 +0900

    アプリの初期化はRuntimeInitializeOnLoadMethodをやめてエントリポイントのAwakeでやるように変更

--- Assets/Holiday/App/AppScope.cs
+++ Assets/Holiday/App/AppScope.cs
@@ -1,4 +1,5 @@
-﻿using Extreal.Core.StageNavigation;
+﻿using Extreal.Core.Logging;
+using Extreal.Core.StageNavigation;
 using Extreal.SampleApp.Holiday.Models;
 using UnityEngine;
 using VContainer;
@@ -9,6 +10,27 @@ namespace Extreal.SampleApp.Holiday.App
     public class AppScope : LifetimeScope
     {
         [SerializeField] private StageConfig stageConfig;
+
+        private static void InitializeApp()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
+
+            const LogLevel logLevel = LogLevel.Debug;
+            LoggingManager.Initialize(logLevel: logLevel);
+
+            var logger = LoggingManager.GetLogger(nameof(AppScope));
+            if (logger.IsDebug())
+            {
+                logger.LogDebug($"targetFrameRage: {Application.targetFrameRate}, logLevel: {logLevel}");
+            }
+        }
+
+        protected override void Awake()
+        {
+            InitializeApp();
+            base.Awake();
+        }
 
         protected override void Configure(IContainerBuilder builder)
         {
