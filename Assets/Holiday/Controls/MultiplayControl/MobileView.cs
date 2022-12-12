b26dda6fb59929e2b61697529335afcecbe6dbee
kiyohome <kiyohito.itoh@gmail.com>
2022-12-12 19:41:44 +0900

    モバイルのUIをMultiplayControlに移動

---
+++ Assets/Holiday/Controls/MultiplayControl/MobileView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Extreal.SampleApp.Holiday.Controls.MultiplayControl
+{
+    public class MobileView : MonoBehaviour
+    {
+        [SerializeField] private GameObject joysticksCanvas;
+
+        private void Awake()
+        {
+            joysticksCanvas.SetActive(false);
+#if UNITY_IOS || UNITY_ANDROID
+            joysticksCanvas.SetActive(true);
+#endif
+        }
+    }
+}
