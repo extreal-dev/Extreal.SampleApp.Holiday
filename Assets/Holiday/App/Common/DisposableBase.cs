6a4af20aea037ff9d6a690f38401ef3721dbb3dd
kiyohome <kiyohito.itoh@gmail.com>
2023-01-24 13:26:15 +0900

    DisposeパターンのBaseクラスを追加

---
+++ Assets/Holiday/App/Common/DisposableBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extreal.SampleApp.Holiday.App.Common
+{
+    public abstract class DisposableBase : IDisposable
+    {
+        private readonly SafeDisposer safeDisposer;
+
+        protected DisposableBase(Action freeManagedResources = null, Action freeUnmanagedResources = null)
+            => safeDisposer = new SafeDisposer(this, freeManagedResources, freeUnmanagedResources);
+
+        ~DisposableBase() => safeDisposer.DisposeByFinalizer();
+
+        public void Dispose() => safeDisposer.Dispose();
+    }
+}
