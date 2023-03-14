0fcb3f53546f15a02c773274b285aaee80b7dd6f
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2023-03-14 13:59:02 +0900

    Remove AppVariables

--- Assets/Holiday/App/AssetWorkflow/Custom/AppVariables.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Extreal.SampleApp.Holiday.App.AssetWorkflow.Custom
-{
-    public static class AppVariables
-    {
-        public static string HolidayVersion => Application.version;
-    }
-}
