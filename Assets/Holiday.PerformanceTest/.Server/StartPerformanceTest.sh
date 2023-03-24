e05e5cfbe5599db5f5308404c5f6335fa4b809c5
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2023-03-24 12:15:37 +0900

    Modify measurement script for performance test

--- Assets/Holiday.PerformanceTest/.Server/StartPerformanceTest.sh
+++ Assets/Holiday.PerformanceTest/.Server/StartPerformanceTest.sh
@@ -4,19 +4,19 @@ exec_time=5400
 client_num_per_server=18
 
 lifetime=`expr $exec_time + $client_num_per_server \* 10 + 120`
-
 date=`date +%Y%m%d`
 
 mkdir -p ../Data/$date
 dir_count=`ls -l ../Data/$date | grep ^d | wc -l`
 
-dir_name=Data/$date/$dir_count
+dir_name="Data/$date/$dir_count"
 work_dir="../$dir_name"
 cpu_memory_file_name="$work_dir/server_CpuMemoryUtilization.csv"
 memory_file_name="../Logs/$date/$dir_count/server_MemoryUtilization.txt"
 
 mkdir $work_dir
 
+./StartServer.sh ../Logs/$date/$dir_count
 ~/dool/dool -Ttcglypmdrn --output $cpu_memory_file_name 1 $lifetime
 ./StopServer.sh
 
