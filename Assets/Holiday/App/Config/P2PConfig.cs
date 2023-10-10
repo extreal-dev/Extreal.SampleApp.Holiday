6419d240422d0931ed05b4eccd89221a0f48e216
Cho Kinken <mahaweilu@gmail.com>
2023-10-10 15:00:24 +0900

    refactoring

--- Assets/Holiday/App/Config/P2PConfig.cs
+++ Assets/Holiday/App/Config/P2PConfig.cs
@@ -13,17 +13,20 @@ namespace Extreal.SampleApp.Holiday.App.Config
     public class P2PConfig : ScriptableObject
     {
         [SerializeField, SuppressMessage("Usage", "CC0052")] private string signalingUrl = "http://127.0.0.1:3010";
-        [SerializeField, SuppressMessage("Usage", "CC0052")] private int timeoutSeconds = 5;
+        [SerializeField, SuppressMessage("Usage", "CC0052")] private int socketConnectionTimeoutSeconds = 5;
         [SerializeField, SuppressMessage("Usage", "CC0052")] private List<string> iceServerUrls = new List<string>();
+        [SerializeField, SuppressMessage("Usage", "CC0052")] private int p2pTimeoutSeconds = 15;
 
         public PeerConfig PeerConfig
             => new PeerConfig(
                 signalingUrl,
                 new SocketIOOptions
                 {
-                    ConnectionTimeout = TimeSpan.FromSeconds(timeoutSeconds),
+                    ConnectionTimeout = TimeSpan.FromSeconds(socketConnectionTimeoutSeconds),
                     Reconnection = false,
                 },
-                iceServerUrls);
+                iceServerUrls,
+                p2pTimeoutSeconds
+                );
     }
 }
