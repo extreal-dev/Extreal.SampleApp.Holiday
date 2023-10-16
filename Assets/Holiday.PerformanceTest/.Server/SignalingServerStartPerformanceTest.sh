874346c299f97f8c15c4aeffc3773ad1efce47a8
Takuya Okita <takuya10@gmail.com>
2023-10-16 16:28:51 +0900

    Delete unuse scripts

--- Assets/Holiday.PerformanceTest/.Server/SignalingServerStartPerformanceTest.sh
+++ Assets/Holiday.PerformanceTest/.Server/SignalingServerStartPerformanceTest.sh
@@ -12,14 +12,12 @@ dir_count=`ls -l ../Data/$date | grep ^d | wc -l`
 dir_name="Data/$date/$dir_count"
 work_dir="../$dir_name"
 cpu_memory_file_name="$work_dir/signalingServer_CpuMemoryUtilization.csv"
-memory_file_name="../Logs/$date/$dir_count/signalingServer_MemoryUtilization.txt"
 
 mkdir $work_dir
 
 ~/dool/dool -Ttcglypmdrn --output $cpu_memory_file_name 1 $lifetime
 
 aws s3 cp $cpu_memory_file_name s3://extreal-webgl/PerformanceTest/$dir_name/
-aws s3 cp $memory_file_name s3://extreal-webgl/PerformanceTest/$dir_name/
 
 sleep 100
 mkdir $work_dir/tmp
