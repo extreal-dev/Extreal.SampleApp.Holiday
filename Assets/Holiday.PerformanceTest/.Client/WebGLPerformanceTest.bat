23a1aeb8bc811f9d793e1c4e564c9b1155a4e1f9
Takuya Okita <takuya10@gmail.com>
2023-10-31 09:57:17 +0900

    Fix output path

--- Assets/Holiday.PerformanceTest/.Client/WebGLPerformanceTest.bat
+++ Assets/Holiday.PerformanceTest/.Client/WebGLPerformanceTest.bat
@@ -2,8 +2,8 @@
 setlocal
 
 REM set exec_time=5400
-set exec_time=10
-set client_num=2
+set exec_time=3800
+set client_num=9
 
 
 set /a player_lifetime=%exec_time%+60
@@ -11,13 +11,14 @@ set /a get_resource_lifetime=%player_lifetime%+%client_num%*10
 
 set my_date=%date:/=%
 
-mkdir ..\Data\%my_date% > NUL 2>&1
 for /f "usebackq delims=" %%A in (`dir /AD /B ..\Data\%my_date% ^| find /c /v ""`) do set dir_count=%%A
+mkdir ..\Data\%my_date%\%dir_count% > NUL 2>&1
 
 set file_name=WebGLResourceUtilization.csv
-echo %file_name%
+set file_path=..\Data\%my_date%\%dir_count%\
+echo %file_path%%file_name%
 
-typeperf -si 1 -sc %get_resource_lifetime% -o %file_name% -y "\processor(_Total)\%% Processor Time" "\Memory\Available Bytes" "\Network Interface(Realtek Gaming 2.5GbE Family Controller)\Bytes Received/sec" "\Network Interface(Realtek Gaming 2.5GbE Family Controller)\Bytes Sent/sec"
+typeperf -si 1 -sc %get_resource_lifetime% -o %file_path%%file_name% -y "\processor(_Total)\%% Processor Time" "\Memory\Available Bytes" "\Network Interface(Realtek Gaming 2.5GbE Family Controller)\Bytes Received/sec" "\Network Interface(Realtek Gaming 2.5GbE Family Controller)\Bytes Sent/sec"
 
 timeout 100
 
