59547bd3435f06052b1a3ee74fe2afb13a2d838f
Takuya Okita <takuya10@gmail.com>
2023-10-31 11:14:38 +0900

    Remove automatic aggregation of memory usage

--- Assets/Holiday.PerformanceTest/.Server/SignalingServerStartPerformanceTest.sh
+++ Assets/Holiday.PerformanceTest/.Server/SignalingServerStartPerformanceTest.sh
@@ -18,16 +18,3 @@ mkdir $work_dir
 ~/dool/dool -Ttcglypmdrn --output $cpu_memory_file_name 1 $lifetime
 
 aws s3 cp $cpu_memory_file_name s3://extreal-webgl/PerformanceTest/$dir_name/
-
-sleep 100
-mkdir $work_dir/tmp
-aws s3 cp --recursive s3://extreal-webgl/PerformanceTest/$dir_name/ $work_dir/tmp/
-
-for i in `seq 1 $((client_num_per_server - 1))`
-do
-    python3 DataAnalysis.py $work_dir/tmp/_MemoryUtilization${i}\*
-    aws s3 cp $work_dir/tmp/_MemoryUtilization${i}.csv s3://extreal-webgl/PerformanceTest/$dir_name/
-done
-
-mv $work_dir/tmp/NGO_Client_00?_MemoryUtilization.csv $work_dir
-rm -rf $work_dir/tmp
