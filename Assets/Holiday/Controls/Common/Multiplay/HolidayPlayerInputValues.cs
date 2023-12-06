1ae4350e608de16f044a7d729c1fc64e732df621
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2023-12-06 16:08:07 +0900

    Enable to control whether to send synchronized values on application

--- Assets/Holiday/Controls/Common/Multiplay/HolidayPlayerInputValues.cs
+++ Assets/Holiday/Controls/Common/Multiplay/HolidayPlayerInputValues.cs
@@ -7,22 +7,57 @@ namespace Extreal.SampleApp.Holiday.Controls.Common.Multiplay
     [Serializable]
     public class HolidayPlayerInputValues : MultiplayPlayerInputValues
     {
+        private Vector2 preMove;
+        private bool isMoveChanged;
+
         public bool Sprint => sprint;
         [SerializeField] private bool sprint;
+        private bool preSprint;
+        private bool isSprintChanged;
 
         public bool Jump => jump;
         [SerializeField] private bool jump;
+        private bool preJump;
+        private bool isJumpChanged;
 
         public bool InputFieldTyping => inputFieldTyping;
         [SerializeField] private bool inputFieldTyping;
+        private bool preInputFieldTyping;
+        private bool isInputFieldTypingChanged;
+
+        public override void SetMove(Vector2 move)
+        {
+            preMove = Move;
+            base.SetMove(move);
+            isMoveChanged = preMove != Move;
+        }
 
         public void SetSprint(bool sprint)
-            => this.sprint = sprint;
+        {
+            preSprint = this.sprint;
+            this.sprint = sprint;
+            isSprintChanged = preSprint != this.sprint;
+        }
 
         public void SetJump(bool jump)
-            => this.jump = jump;
+        {
+            preJump = this.jump;
+            this.jump = jump;
+            isJumpChanged = preJump != this.jump;
+        }
 
         public void SetInputFieldTyping(bool inputFieldTyping)
-            => this.inputFieldTyping = inputFieldTyping;
+        {
+            preInputFieldTyping = this.inputFieldTyping;
+            this.inputFieldTyping = inputFieldTyping;
+            isInputFieldTypingChanged = preInputFieldTyping != this.inputFieldTyping;
+        }
+
+        public override bool CheckWhetherToSendData()
+        {
+            var ret = isMoveChanged || isSprintChanged || isJumpChanged || isInputFieldTypingChanged;
+            isMoveChanged = isSprintChanged = isJumpChanged = isInputFieldTypingChanged = false;
+            return ret;
+        }
     }
 }
