ccc491b90a9dd04b8083c684447e094c78d20a9b
Cho Kinken <mahaweilu@gmail.com>
2024-01-09 16:59:17 +0900

    remove ngo.webrtc

--- Assets/WebGLScripts~/src/index.ts
+++ Assets/WebGLScripts~/src/index.ts
@@ -1,6 +1,5 @@
 import { PeerAdapter } from "@extreal-dev/extreal.integration.p2p.webrtc";
-import { WebRtcAdapter } from "@extreal-dev/extreal.integration.multiplay.ngo.webrtc";
-import { TextChatAdapter, VoiceChatAdapter } from "@extreal-dev/extreal.integration.chat.webrtc";
+import { VoiceChatAdapter } from "@extreal-dev/extreal.integration.chat.webrtc";
 import { addFunction, isDebug } from "@extreal-dev/extreal.integration.web.common";
 import { isTouchDevice } from "./isTouchDevice";
 import { RedisMessagingAdapter } from "@extreal-dev/extreal.integration.messaging.redis";
